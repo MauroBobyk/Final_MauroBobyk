c12a43beae20f09f1c4a6f78b121607dffa2ca9a
Mauro <mauro.bobyk@gmail.com>
2023-11-15 13:53:21 -0300

    13:45

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::view('PanelUsuario',"PanelUsuario")->name('PanelUsuario');
+route::post('')
